1cd8996280a72bbe1262af0e4bfe9ff9f236869c
blackpointdev <marcinw1998@gmail.com>
2020-04-07 19:16:13 +0200

    Fix lcd display.

--- src/app.py
+++ src/app.py
@@ -4,5 +4,6 @@ from lcd_driver.lcd_display_driver import lcd
 data_source = GlobalCases()
 lcd_display = lcd()
 
-print(data_source.get_total_infection_cases())
-lcd_display.lcd_display_string(data_source.get_total_infection_cases, 1)
+cases = str(data_source.get_total_infection_cases())
+print(cases)
+lcd_display.lcd_display_string(cases, 1)
