f2aa7594e5545210e88fb2e239cfed0595503ad8
blackpointdev <marcinw1998@gmail.com>
2020-04-07 19:11:56 +0200

    Added LCD display driver.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 from data_sources.global_cases import GlobalCases
+from lcd_driver.lcd_display_driver import lcd
 
 data_source = GlobalCases()
+lcd_display = lcd()
 
 print(data_source.get_total_infection_cases())
+lcd_display.lcd_display_string(data_source.get_total_infection_cases, 1)
