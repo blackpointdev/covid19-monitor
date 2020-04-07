2a7f69282ffb29fa8c26a2c799b7f46667bd0ec7
blackpointdev <marcinw1998@gmail.com>
2020-04-07 19:18:58 +0200

    Fixed display.

--- src/app.py
+++ src/app.py
@@ -4,6 +4,10 @@ from lcd_driver.lcd_display_driver import lcd
 data_source = GlobalCases()
 lcd_display = lcd()
 
-cases = str(data_source.get_total_infection_cases())
+cases = data_source.get_total_infection_cases()
+
 print(cases)
-lcd_display.lcd_display_string(cases, 1)
+
+lcd_display.lcd_display_string("All: " + str(cases.all_cases), 1)
+lcd_display.lcd_display_string("Deaths: " + str(cases.deaths), 2)
+lcd_display.lcd_display_string("Recovered: " + str(cases.recovered), 3)
